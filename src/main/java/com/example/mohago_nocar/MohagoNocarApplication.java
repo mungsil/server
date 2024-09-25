0cc8ef7a78714bcbcc0cf1ec2ce633bab4f94a11
mingmingmon <mingmingmon@naver.com>
2024-09-25 16:52:21 +0900

    setting: 스프링부트 세팅 #1

---
+++ src/main/java/com/example/mohago_nocar/MohagoNocarApplication.java
@@ -0,0 +1,13 @@
+package com.example.mohago_nocar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohagoNocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohagoNocarApplication.class, args);
+	}
+
+}
