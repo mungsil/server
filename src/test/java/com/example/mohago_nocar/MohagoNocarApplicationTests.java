218f788c2edcabb05aaa60f5dd5822dc32d15e38
mingmingmon <96719969+mingmingmon@users.noreply.github.com>
2024-09-26 09:34:57 +0900

    setting: 스프링부트 세팅 완료 (#4)
    * setting: 스프링부트 세팅 #1
    
    * setting: dev yml 파일 내용 수정 #1
    
    * setting: build.gradle 용도 그룹화 및 주석 #1

---
+++ src/test/java/com/example/mohago_nocar/MohagoNocarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mohago_nocar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohagoNocarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
